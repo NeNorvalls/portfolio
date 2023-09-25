d76021a26206861d1c936108ef7d03d88857bd21
Crystaliner1000 <honeygeeh27@gmail.com>
2023-09-25 21:48:23 +0200

    linking all pages to the App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 import "./App.css";
+import Skills from "./components/Skills/Skills";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Skills />
+      <AboutMe />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
